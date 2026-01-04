fc932d444368283dd424445d8b03314cb59bd0a7
Suthinan Rongphon <suthinan.rpn@gmail.com>
2026-01-04 23:33:44 +0700

    feat: add max() function

--- 02-built-in/01_math_numbers/math_min_max.py
+++ 02-built-in/01_math_numbers/math_min_max.py
@@ -1,7 +1,10 @@
 # ==========================================
 # min() Function
 # Syntax: min(arg1, arg2, arg3, ...) or min(iterable)
+# Return the smallest item in an iterable or the smallest of two or more arguments.
 # ==========================================
+
+print("--- min() Function ---")
 
 print("--- List numbers ---")
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -15,3 +18,20 @@ print(f"List animal: {min(animal)}")  # cat
 print("--- Set ---")
 numbers = (11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 print(f"Set min value: {min(numbers)}")  # 11
+
+
+# ==========================================
+# max() Function
+# Syntax: min(arg1, arg2, arg3, ...) or min(iterable)
+# Return the largest item in an iterable or the largest of two or more arguments.
+# ==========================================
+
+print("--- max() Function ---")
+
+print("--- List numbers ---")
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+number_set01 = 1
+number_set02 = 20
+number_set03 = 33
+
+print(f"List max value: {max(number_set01, number_set02, number_set03)}")  # 1
