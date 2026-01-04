b0d55b2269a6fb5a75db532abc5b261ee2f26ca4
Suthinan Rongphon <suthinan.rpn@gmail.com>
2026-01-04 23:29:17 +0700

    feat: add min() Function

---
+++ 02-built-in/01_math_numbers/math_min_max.py
@@ -0,0 +1,17 @@
+# ==========================================
+# min() Function
+# Syntax: min(arg1, arg2, arg3, ...) or min(iterable)
+# ==========================================
+
+print("--- List numbers ---")
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(f"List min value: {min(numbers)}")  # 1
+
+print("--- List words ---")
+animal = ["cow", "dog", "cat", "owl"]
+print(f"List animal: {min(animal)}")  # cat
+
+
+print("--- Set ---")
+numbers = (11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+print(f"Set min value: {min(numbers)}")  # 11
